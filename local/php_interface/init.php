31ee0662dff7db83c596baf8cb74e1b0d61829dc
Vitaly Yukovich <parker17.1994@gmail.com>
2020-07-01 14:58:55 +0300

    Загрузка задания ex2-93

---
+++ local/php_interface/init.php
@@ -0,0 +1,25 @@
+<?
+AddEventHandler('main', 'OnEpilog', array("MyHandler", "checkNonExistingPage"));
+
+class MyHandler
+{
+    function checkNonExistingPage(){
+        if (defined('ERROR_404') && ERROR_404 == 'Y') {
+            global $APPLICATION;
+            $APPLICATION->RestartBuffer();
+            include $_SERVER['DOCUMENT_ROOT'] . SITE_TEMPLATE_PATH . '/header.php';
+            include $_SERVER['DOCUMENT_ROOT'] . '/404.php';
+            include $_SERVER['DOCUMENT_ROOT'] . SITE_TEMPLATE_PATH . '/footer.php';
+
+            CEventLog::Add(
+                array(
+                    "SEVERITY"      => "INFO",
+                    "AUDIT_TYPE_ID" => "ERROR_404",
+                    "MODULE_ID"     => "main",
+                    "DESCRIPTION"   => $_SERVER['DOCUMENT_ROOT'] . $APPLICATION->GetCurUri(),
+                )
+            );
+        }
+    }
+}
+?>
