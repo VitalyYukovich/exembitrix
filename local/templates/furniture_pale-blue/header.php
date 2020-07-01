665a3ab756b0b9c7bb42f02f3c998eb7926568ef
Vitaly Yukovich <parker17.1994@gmail.com>
2020-07-01 15:17:03 +0300

    Загрузка задания ex2-94

--- local/templates/furniture_pale-blue/header.php
+++ local/templates/furniture_pale-blue/header.php
@@ -7,22 +7,33 @@ IncludeTemplateLangFile(__FILE__);
 <head>
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <?$APPLICATION->ShowHead();?>
+
+<?
+	CModule::IncludeModule('iblock');
+	$result = CIBlockElement::GetList(array("SORT"=>"ASC"), array('IBLOCK_ID'=>6, 'NAME' => $APPLICATION->getCurDir()), false, false, array('ID', 'NAME', 'PROPERTY_TITLE', 'PROPERTY_DESCRIPTION'));
+	while($item = $result->GetNextElement()){
+		$itemGetFields = $item->GetFields();
+		$APPLICATION->SetPageProperty('title', $itemGetFields['PROPERTY_TITLE_VALUE']);
+		$APPLICATION->SetPageProperty('description', $itemGetFields['PROPERTY_DESCRIPTION_VALUE']);
+	}
+?>
+
 <link href="<?=SITE_TEMPLATE_PATH?>/common.css" type="text/css" rel="stylesheet" />
 <link href="<?=SITE_TEMPLATE_PATH?>/colors.css" type="text/css" rel="stylesheet" />
 
 	<!--[if lte IE 6]>
 	<style type="text/css">
-		
-		#banner-overlay { 
+
+		#banner-overlay {
 			background-image: none;
-			filter: progid:DXImageTransform.Microsoft.AlphaImageLoader(src='<?=SITE_TEMPLATE_PATH?>images/overlay.png', sizingMethod = 'crop'); 
+			filter: progid:DXImageTransform.Microsoft.AlphaImageLoader(src='<?=SITE_TEMPLATE_PATH?>images/overlay.png', sizingMethod = 'crop');
 		}
-		
+
 		div.product-overlay {
 			background-image: none;
 			filter: progid:DXImageTransform.Microsoft.AlphaImageLoader(src='<?=SITE_TEMPLATE_PATH?>images/product-overlay.png', sizingMethod = 'crop');
 		}
-		
+
 	</style>
 	<![endif]-->
 
@@ -32,7 +43,7 @@ IncludeTemplateLangFile(__FILE__);
 	<div id="page-wrapper">
 	<div id="panel"><?$APPLICATION->ShowPanel();?></div>
 		<div id="header">
-			
+
 			<table id="logo">
 				<tr>
 					<td><a href="<?=SITE_DIR?>" title="<?=GetMessage('CFT_MAIN')?>"><?
@@ -44,7 +55,7 @@ $APPLICATION->IncludeFile(
 ?></a></td>
 				</tr>
 			</table>
-			
+
 			<div id="top-menu">
 				<div id="top-menu-inner">
 <?$APPLICATION->IncludeComponent("bitrix:menu", "horizontal_multilevel", array(
@@ -64,16 +75,16 @@ $APPLICATION->IncludeFile(
 );?>
 				</div>
 			</div>
-			
+
 			<div id="top-icons">
 				<a href="<?=SITE_DIR?>" class="home-icon" title="<?=GetMessage('CFT_MAIN')?>"></a>
 				<a href="<?=SITE_DIR?>search/" class="search-icon" title="<?=GetMessage('CFT_SEARCH')?>"></a>
 				<a href="<?=SITE_DIR?>contacts/" class="feedback-icon" title="<?=GetMessage('CFT_FEEDBACK')?>"></a>
 			</div>
-		
+
 		</div>
-		
-		<div id="banner">		
+
+		<div id="banner">
 			<table id="banner-layout" cellspacing="0">
 				<tr>
 					<td id="banner-image"><div><img src="<?=SITE_TEMPLATE_PATH?>/images/head.jpg" /></div></td>
@@ -88,11 +99,11 @@ $APPLICATION->IncludeFile(
 					</td>
 				</tr>
 			</table>
-			<div id="banner-overlay"></div>	
+			<div id="banner-overlay"></div>
 		</div>
-		
+
 		<div id="content">
-		
+
 			<div id="sidebar">
 <?$APPLICATION->IncludeComponent("bitrix:menu", "left", array(
 	"ROOT_MENU_TYPE" => "left",
@@ -123,10 +134,10 @@ $APPLICATION->IncludeFile(
 ?>
 					</div>
 				</div>
-				
+
 				<div class="content-block">
 					<div class="content-block-inner">
-						
+
 <?
 $APPLICATION->IncludeComponent("bitrix:search.form", "flat", Array(
 	"PAGE" => "#SITE_DIR#search/",
@@ -147,11 +158,11 @@ $APPLICATION->IncludeFile(
 	Array(),
 	Array("MODE"=>"html")
 );
-?>						
+?>
 					</div>
 					<div class="bottom"></div>
 				</div>
 			</div>
-		
+
 			<div id="workarea">
 				<h1 id="pagetitle"><?$APPLICATION->ShowTitle(false);?></h1>
