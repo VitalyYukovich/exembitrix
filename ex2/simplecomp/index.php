e8b2c8556e97d458a409e9a2e7741ee3b8f62e3f
Vitaly Yukovich <parker17.1994@gmail.com>
2020-07-08 18:02:23 +0300

    Добавлено кеширование данных

--- ex2/simplecomp/index.php
+++ ex2/simplecomp/index.php
@@ -2,15 +2,13 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Простой компонент");
 ?><?$APPLICATION->IncludeComponent(
-	"simplenamespace:simplecomp.exam", 
-	".default", 
-	array(
-		"COMPONENT_TEMPLATE" => ".default",
-		"IBLOCK_ID_PRODUCT" => "2",
-		"IBLOCK_ID_NEWS" => "1",
-		"CODE_USER_PROPERTY" => "UF_NEWS_LINK",
+	"simplenamespace:simplecomp.exam",
+	"",
+	Array(
+		"CACHE_TIME" => "36000",
 		"CACHE_TYPE" => "A",
-		"CACHE_TIME" => "0"
-	),
-	false
+		"CODE_USER_PROPERTY" => "UF_NEWS_LINK",
+		"IBLOCK_ID_NEWS" => "1",
+		"IBLOCK_ID_PRODUCT" => "2"
+	)
 );?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
