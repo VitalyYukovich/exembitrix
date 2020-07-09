853f238a66807ddaf91b7a059c93bcc1029772c2
Vitaly Yukovich <parker17.1994@gmail.com>
2020-07-09 10:18:35 +0300

    ex2-88

---
+++ ex2/time_control/index.php
@@ -0,0 +1,9 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Оценка производительности");
+?>
+
+<p>Самая нагруженная страница сайта <a href='/company/index.php'>/company/index.php</a>, доля ее нагрузки составляет 21.28%</p>
+<p>Кеш компоненты 'Каталог товаров [ex2-71]' - 0</p>
+<img src = '/upload/cache.jpg' alt='cachesimplecomp'>
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
