f5d0295eef5a93200db3d3b6b612366fe4d67c3d
Vitaly Yukovich <parker17.1994@gmail.com>
2020-07-10 17:47:12 +0300

    удалено показывание свойство cannonical отдельно

--- local/templates/furniture_pale-blue/header.php
+++ local/templates/furniture_pale-blue/header.php
@@ -7,7 +7,6 @@ IncludeTemplateLangFile(__FILE__);
 <head>
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <?$APPLICATION->ShowHead();?>
-<?$APPLICATION->ShowProperty("canonical");?>
 <link href="<?=SITE_TEMPLATE_PATH?>/common.css" type="text/css" rel="stylesheet" />
 <link href="<?=SITE_TEMPLATE_PATH?>/colors.css" type="text/css" rel="stylesheet" />
 
