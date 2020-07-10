1305f724147d9fed2e468283e89d01f4ffc3e815
Vitaly Yukovich <parker17.1994@gmail.com>
2020-07-10 20:04:20 +0300

    Убран вывод 404 страницы

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -6,10 +6,6 @@ class MyHandler
     function checkNonExistingPage(){
         if (defined('ERROR_404') && ERROR_404 == 'Y') {
             global $APPLICATION;
-            $APPLICATION->RestartBuffer();
-            include $_SERVER['DOCUMENT_ROOT'] . SITE_TEMPLATE_PATH . '/header.php';
-            include $_SERVER['DOCUMENT_ROOT'] . '/404.php';
-            include $_SERVER['DOCUMENT_ROOT'] . SITE_TEMPLATE_PATH . '/footer.php';
 
             CEventLog::Add(
                 array(
