cf347f674818cdebfc01d0c66e9e38684cd94a8c
Vitaly Yukovich <parker17.1994@gmail.com>
2020-07-10 12:23:36 +0300

    Удаление формирование ссылки на страницу детального просмотра

--- local/components/simplenamespace/simplecomp.exam/templates/.default/template.php
+++ local/components/simplenamespace/simplecomp.exam/templates/.default/template.php
@@ -1,18 +1,12 @@
-<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
-
-	$arTemplateLinkDetail = explode('/', $arParams['TEMPLATE_LINK_DETAIL']);
-	array_shift($arTemplateLinkDetail);
-?>
+<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();?>
 	<h3><b>Каталог:</b></h3>
 
 	<ul>
 	<?foreach($arResult['arClassifier'] as $classifier){?>
 		<li><b><?echo $classifier['NAME'];?></b></li>
 		<ul>
-			<?foreach($classifier['PRODUCT'] as $product){
-				$linkDetail = $arTemplateLinkDetail[0] . '/' . $product[trim($arTemplateLinkDetail[1], '#')] . '/' . $product[trim($arTemplateLinkDetail[2], '#')] . '/';
-				?>
-				<li><?echo $product['NAME'] . ' - ' . $product['PROPERTY_PRICE_VALUE'] . ' - ' . $product['PROPERTY_MATERIAL_VALUE'] . ' - ' . $product['PROPERTY_ARTNUMBER_VALUE'] . ' - ' . $linkDetail;
+			<?foreach($classifier['PRODUCT'] as $product){?>
+				<li><?echo $product['NAME'] . ' - ' . $product['PROPERTY_PRICE_VALUE'] . ' - ' . $product['PROPERTY_MATERIAL_VALUE'] . ' - ' . $product['PROPERTY_ARTNUMBER_VALUE'];
 				?></li>
 			<?}?>
 		</ul>
